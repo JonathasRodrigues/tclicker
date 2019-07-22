b179e22c761b39a14b1324dd8249c50653e16145
byttar <nadaletriel@gmail.com>
2019-07-21 21:59:34 -0300

    menu items comprados

--- src/components/ListMyItems.tsx
+++ src/components/ListMyItems.tsx
@@ -1,24 +1,96 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { IItem } from '../classes/Item';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { IItem } from "../classes/Item";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import { withStyles, createMuiTheme } from "@material-ui/core/styles";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+
+const styles = {
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: createMuiTheme().palette.background.paper
+  },
+  nested: {
+    paddingLeft: createMuiTheme().spacing(4)
+  },
+  inline: {
+    display: "inline"
+  },
+  avatar: {
+    margin: 10
+  }
+};
 
 class ListMyItems extends Component<any, any> {
   render() {
-    const { list } = this.props;
+    const { list, classes } = this.props;
     return (
       <React.Fragment>
-        {Array.isArray(list) ? list.map((obj: IItem) => (
-          <p key={`My-item-${obj.id}`}>{obj.quantity} || Nome: {obj.name} - Valor: {obj.price}</p>
-        )) : 'Você não tem Items'}
+        <List
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          subheader={
+            <ListSubheader component="div" id="nested-list-subheader">
+              Itens Comprados
+            </ListSubheader>
+          }
+          className={classes.root}
+        >
+          {Array.isArray(list) && list.length > 0 ? (
+            list.map((obj: IItem) => (
+              <React.Fragment>
+                <Divider />
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar
+                      alt={`Item ${obj.name} image`}
+                      src={obj.image}
+                      className={classes.avatar}
+                    />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={obj.name}
+                    secondary={
+                      <React.Fragment>
+                        <Typography
+                          component="p"
+                          variant="body2"
+                          className={classes.inline}
+                          color="textPrimary"
+                        >
+                          Você possui
+                          {` ${obj.quantity} ${obj.name}(s)`}
+                        </Typography>
+                      </React.Fragment>
+                    }
+                  />
+                  {/* <p key={`My-item-${obj.id}`}>
+                    {obj.quantity} || Nome: {obj.name} - Valor: {obj.price}
+                  </p> */}
+                </ListItem>
+              </React.Fragment>
+            ))
+          ) : (
+            <React.Fragment>
+              <Divider />
+              <ListItem>Não há items</ListItem>
+            </React.Fragment>
+          )}
+        </List>
       </React.Fragment>
     );
   }
 }
 
 function mapDispatchToProps(dispatch: any) {
-  return {
-
-  }
+  return {};
 }
 
 function mapStateToProps(state: any) {
@@ -26,7 +98,10 @@ function mapStateToProps(state: any) {
   return {
     list: MyItems,
     count: MainCount
-  }
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ListMyItems);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(ListMyItems));
